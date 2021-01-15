5c0658f51037e8bad3d2c3a3ea0b8100c7229ac8
adorjan87 <tadorjan@gmil.om>
2021-01-15 23:57:04 +0100

    Add description

--- tablespace_check.sql
+++ tablespace_check.sql
@@ -1,8 +1,8 @@
 COLUMN TS_name FORMAT a12
 select
 tablespace_name AS TS_name,
-round(sum(bytes)/1024/1024,2) AS Actual_size_M,
-round(sum(maxbytes)/1024/1024,2) AS Max_size_M,
+round(sum(bytes)/1024/1024,2) AS Alloctaed_MB,
+round(sum(maxbytes)/1024/1024,2) AS Max_size_MB,
 round((sum(bytes)/sum(maxbytes))*100,3) AS "Saturation_%"
 from dba_data_files
 group by tablespace_name
