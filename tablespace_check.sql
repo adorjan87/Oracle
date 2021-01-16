27ce0d9807bd5b95d235f9ddbd6ca940547c43ea
adorjan87 <tadorjan@gmil.om>
2021-01-16 12:27:16 +0100

    replace script

--- tablespace_check.sql
+++ tablespace_check.sql
@@ -1,9 +1,32 @@
-COLUMN TS_name FORMAT a12
+with ts_total (ts_name, df_total, df_max) as
+    (
+    select
+        tablespace_name,
+        sum(bytes),
+        sum(maxbytes)
+    from dba_data_files
+    group by tablespace_name
+    ),
+    ts_free_space(ts_name,ts_free_space) as
+    (
+    select
+        tablespace_name,
+        nvl(sum(bytes),0)
+    from dba_free_space
+    group by tablespace_name
+    )
+
 select
-tablespace_name AS TS_name,
-round(sum(bytes)/1024/1024,2) AS Alloctaed_MB,
-round(sum(maxbytes)/1024/1024,2) AS Max_size_MB,
-round((sum(bytes)/sum(maxbytes))*100,3) AS "Saturation_%"
-from dba_data_files
-group by tablespace_name
+    tst.ts_name as "TS name",
+    round(tst.df_total/1024/1024,2) as "Physical size (MB)", --Datafile real physical size in filesystem
+    round((tst.df_total - tsf.ts_free_space)/1024/1024,2) as "Used (MB)", --Real useges from TS
+    round(tsf.ts_free_space/1024/1024,2)  as "Free in TS (MB)", -- Actual free space in TS
+    round(((tst.df_total - tsf.ts_free_space) / tst.df_total) * 100, 2) as "Current usages (%)",
+    round(((tst.df_total - tsf.ts_free_space) / tst.df_max) * 100, 2) as "Real usages (%)" -- It is a real usages in %. It use dba_data_files.maxbytes
+from
+    ts_total tst,
+    ts_free_space tsf
+where
+    tst.ts_name = tsf.ts_name
+order by 6 desc
 ;
