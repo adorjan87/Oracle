001c4c657e3eac393ca6a1a644567f60b0249565
adorjan87 <tadorjan@gmil.om>
2021-03-28 22:00:38 +0200

    add new notice

--- tablespace_check.sql
+++ tablespace_check.sql
@@ -1,10 +1,14 @@
 --This script shows TS usages
-PROMPT =============================================================
+PROMPT
+PROMPT ====================================================================
 PROMPT TS name: Tablespace ts_name
 PROMPT TS can grow (MB):  TS max physical what able to achieve
-PROMPT Physical size (MB): TS actual real physical size
-PROMPT Used (MB): Ral usages in TS. This size depend on autoextend option
-PROMPT =============================================================
+PROMPT Physical size (MB): TS actual real physical size. Actually it is all datafiles size in filesystem.
+PROMPT Used (MB): Real usages in TS. This size depend on autoextend option
+PROMPT: Free in TS (MB): Real free space in TS. Max size is allays autoextended max size
+PROMPT: Current usages (%): It is a physical usages in filesystem.
+PROMPT: Real usages (%): It is a real usages in tablespace.
+PROMPT ====================================================================
 PROMPT
 
 set lines 9999
@@ -35,7 +39,8 @@ select
     round((tst.df_total - tsf.ts_free_space)/1024/1024,2) as "Used (MB)", --Real usages from TS
     round(tsf.ts_free_space/1024/1024,2)  as "Free in TS (MB)", -- Actual free space in TS
     round(((tst.df_total - tsf.ts_free_space) / tst.df_total) * 100, 2) as "Current usages (%)", --Usages in % where max size is a current, actual size
-    round(((tst.df_total - tsf.ts_free_space) / tst.df_max) * 100, 2) as "Real usages (%)" -- It is a real usages in %. It uses dba_data_files.maxbytes
+    round(((tst.df_total / tst.df_max) * 100), 2) as "Real usages (%)" -- It is a real usages in %. It uses dba_data_files.maxbytes
+--round(((tst.df_total - tsf.ts_free_space) / tst.df_max) * 100, 2) as "Real usages (%)"
 from
     ts_total tst,
     ts_free_space tsf
